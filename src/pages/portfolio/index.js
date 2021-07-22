72cdb631b2c983dcb96dbfcab850fa529d6c7311
brandon-desk <brosini9@gmail.com>
2021-07-22 19:06:11 -0400

    add portfolio page

---
+++ src/pages/portfolio/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+export default () => (
+  <Layout>
+    <section className="section">
+      <div className="container">
+        <div className="content">
+          <h1>Welcome to the portfolio section</h1>
+        </div>
+      </div>
+    </section>
+  </Layout>
+)
+
